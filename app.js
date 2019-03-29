69c5a73904fc9a332a50d82ca9e3a14fb7464444
ainter21 <ainter21.6@gmail.com>
2019-03-29 16:26:12 +0100

    Add access counter

--- app.js
+++ app.js
@@ -6,22 +6,29 @@ const PORT = process.env.PORT || 3000;
 
 
 let app_counter = 0;
-
+let access_counter = 0;
 
 app.get('/newUser', (req,res) => {
 
 
     app_counter++;
-    console.log(app_counter);
+    access_counter++;
+    // console.log("app_counter : " + app_counter);
     res.status(200).end();
 });
 
 app.get('/appCounter', (req,res) => {
 
     res.json({
-        "app_counter" : app_counter
+        "app_counter" : app_counter,
+        "access_counter": access_counter
     });
-})
+});
+
+app.get('/newAccess', (req, res) => {
+    access_counter++;
+    res.status(200).end();
+});
 
 
 app.listen(PORT, () => {
